4cd482ec9125c8b0b6d8a6d09451fc57f478f956
Amriteshwar Dwivedi <60342529+damriteshwar@users.noreply.github.com>
2024-11-02 23:59:20 +0530

    Python Practice Files

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+'''
+Author: Amrit
+Licenced to: amrit.co
+***********Thanks for reading**********
+'''
+import os # importing the os module 
+print("Hello world")
---
+++ 02_pr_01_twinkle.py
@@ -0,0 +1,9 @@
+'''
+This problem is a solution of writing Poem
+'''
+# This is also a comment just like the above line
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
